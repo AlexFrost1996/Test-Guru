56173b83d064378cfd6abef5677ae1d7f7bedb4a
Alex <pentagon-1996@tut.by>
2021-09-13 15:27:36 +0300

    add fix (lesson3)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_151229) do
+ActiveRecord::Schema.define(version: 2021_09_13_122450) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2021_09_10_151229) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
