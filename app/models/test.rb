f6e121f8982819e0ed3ec6459acfe0a488727c13
Alex <pentagon-1996@tut.by>
2021-09-13 19:51:24 +0300

    add fix lesson4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   def self.test_titles_in_category(category)
-    Test.joins(:category)
+    Test.joins("JOIN categories ON tests.category_id = categories.id")
         .where(categories: { title: category } )
-        .order(title: :desc)
-        .pluck(:title)
+        .order("categories.title DESC")
+        .pluck("tests.title")
   end
 end
