df106d513d9098b0111b4ce3912f71ebd3f4e657
Alex <pentagon-1996@tut.by>
2021-09-15 23:25:16 +0300

    add change model test lesson5

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
-  belongs_to :author, class_name: 'User', foreign_key: :creator_id
+  belongs_to :creator, class_name: 'User', foreign_key: :creator_id
 
   def self.test_titles_in_category(category)
     Test.joins(:category)
