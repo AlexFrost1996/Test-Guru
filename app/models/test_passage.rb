320d630c5cf84cc911372cb86a25bd96c23c0509
Alex <pentagon-1996@tut.by>
2021-09-28 19:51:24 +0300

    any fix after code review

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -6,6 +6,8 @@ class TestPassage < ApplicationRecord
   before_validation :before_validation_set_first_question, on: :create
   before_validation :before_validation_set_next_question, on: :update
 
+  TEST_COMPLITED = 85
+
   def complited?
     current_question.nil?
   end
@@ -18,7 +20,7 @@ class TestPassage < ApplicationRecord
   end
 
   def succeeded?
-    success_percent >= 85
+    success_percent >= TEST_COMPLITED
   end
 
   def success_percent
