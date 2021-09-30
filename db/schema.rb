baee8c768751d3a128cbc3afb6a140a83a40b5a6
Alex <pentagon-1996@tut.by>
2021-09-30 18:49:33 +0300

    add unique index for email (lesson10)

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_172916) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
