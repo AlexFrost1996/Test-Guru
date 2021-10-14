2ec005a485bb85f901ab163d60a7389742d4cbab
Alex <pentagon-1996@tut.by>
2021-10-14 20:40:38 +0300

    add password notification by JS (lesson15)

---
+++ app/javascript/utilities/passwordNotification.js
@@ -0,0 +1,28 @@
+document.addEventListener('turbolinks:load', function() {
+  var password = document.getElementById('user_password')
+  var password_confirmation = document.getElementById('user_password_confirmation')
+
+  if (password != null && password_confirmation != null) { 
+    password.addEventListener('input', showNotification)  
+    password_confirmation.addEventListener('input', showNotification) 
+  }
+})
+
+function showNotification (){
+
+  var password = document.getElementById('user_password').value
+  var password_confirmation = document.getElementById('user_password_confirmation').value
+  var icon = document.querySelector('.octicon-check')
+  if (password_confirmation == ""){
+    icon.classList.add('no-confirmation')
+    icon.classList.add('hide')
+    return
+  } else if (password === password_confirmation && password_confirmation != "") {
+    icon.classList.remove('no-confirmation')
+    icon.classList.remove('hide')
+
+  } else if (password != password_confirmation && password_confirmation != "") {
+    icon.classList.add('no-confirmation')
+    icon.classList.remove('hide')
+  }
+} 
